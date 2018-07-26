844bfe44a5f785a771ff8ba66a8bfa628fd87185
Thomas Thiry <thomas.thiry@gmail.com>
2018-07-26 21:17:56 +0200

    Added all employee fields to show edit form

--- src/PartsUnlimited.HRBenefits.Web/ViewModels/EmployeeViewModel.cs
+++ src/PartsUnlimited.HRBenefits.Web/ViewModels/EmployeeViewModel.cs
@@ -1,9 +1,22 @@
-﻿namespace PartsUnlimited.HRBenefits.Web.ViewModels
+﻿using System;
+
+namespace PartsUnlimited.HRBenefits.Web.ViewModels
 {
     public class EmployeeViewModel
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Reference { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string AddressNumber { get; set; }
+        public string AddressStreet { get; set; }
+        public string AddressCity { get; set; }
+        public string AddressPostalCode { get; set; }
+        public string AddressCountry { get; set; }
+        public DateTime JoinedCompanyDate { get; set; }
+        public decimal GrossMonthlySalary { get; set; }
+        public bool IsGrantedCar { get; set; }
+        public int NbDaysYearlyHolidays { get; set; }
     }
 }

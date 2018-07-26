76a535352e508c3baffe2eb1938fddab503f783b
Thomas Thiry <thomas.thiry@gmail.com>
2018-07-26 20:15:47 +0200

    Added a first component test

---
+++ tests/PartsUnlimited.HRBenefits.ComponentTests/EmployeeTests.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using PartsUnlimited.HRBenefits.Application.Interfaces.Infrastructure;
+using PartsUnlimited.HRBenefits.Application.Services;
+using PartsUnlimited.HRBenefits.Domain.Entities;
+using PartsUnlimited.HRBenefits.Web.Configuration;
+using PartsUnlimited.HRBenefits.Web.Controllers;
+using PartsUnlimited.HRBenefits.Web.ViewModels;
+using Shouldly;
+using Xunit;
+
+namespace PartsUnlimited.HRBenefits.ComponentTests
+{
+    public class EmployeeTests
+    {
+        [Fact]
+        public void Employees_OneEmployee_ReturnsTheEmployee()
+        {
+            var employeeRepositoryMock = new EmployeeRepositoryMock();
+            employeeRepositoryMock.Employees.Add(new Employee{ Id = 1, FirstName = "Dale", LastName = "Cooper" });
+            var controller = new HomeController(new EmployeeService(employeeRepositoryMock), MapperConfig.CreateMapper());
+
+            var result = controller.Employees() as ViewResult;
+            var employeesViewModel = result.Model as EmployeesViewModel;
+
+            employeesViewModel.Employees.ShouldHaveSingleItem();
+        }
+    }
+
+    public class EmployeeRepositoryMock : IEmployeeRepository
+    {
+        public List<Employee> Employees = new List<Employee>();
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return Employees;
+        }
+    }
+}

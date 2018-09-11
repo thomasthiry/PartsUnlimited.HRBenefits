d7d055cd31eee742a1686c2621b510cb9e4bdc76
Thomas Thiry <thomas.thiry@gmail.com>
2018-09-11 21:07:45 +0200

    Moved Online Api controller action to the Home controller to hide it

--- src/PartsUnlimited.HRBenefits.Web/Controllers/HomeController.cs
+++ src/PartsUnlimited.HRBenefits.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using PartsUnlimited.HRBenefits.Web.Models;
 
@@ -15,6 +16,25 @@ namespace PartsUnlimited.HRBenefits.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        // Represents an external system's api -- included in this web app to make it easier to manage
+        [Route("externalsystem/baseholidays")]
+        public IActionResult BaseHolidays(int employeeId)
+        {
+            var baseHolidaysForEmployees = new Dictionary<int, int>
+            {
+                { 0, 20 },
+                { 1, 20 },
+                { 2, 19 },
+                { 3, 22 },
+                { 4, 21 },
+                { 5, 19 },
+            };
+
+            var baseHolidays = baseHolidaysForEmployees[employeeId % baseHolidaysForEmployees.Count];
+
+            return new JsonResult(new { BaseHolidays = baseHolidays });
         }
     }
 }

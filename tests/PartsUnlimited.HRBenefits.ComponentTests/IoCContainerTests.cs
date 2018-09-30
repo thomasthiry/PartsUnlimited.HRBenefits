76937903d7f41d0b90e4074ed451077db7cffd8f
Thomas Thiry <thomas.thiry@gmail.com>
2018-09-30 16:22:13 +0200

    Added IoCContainerTests

---
+++ tests/PartsUnlimited.HRBenefits.ComponentTests/IoCContainerTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ObjectPool;
+using PartsUnlimited.HRBenefits.Web;
+using PartsUnlimited.HRBenefits.Web.Controllers;
+using Shouldly;
+using Xunit;
+
+namespace PartsUnlimited.HRBenefits.ComponentTests
+{
+    public class IocContainerTests
+    {
+        [Fact]
+        public void ResolveEmployeeController()
+        {
+            var startup = new Startup(null);
+            var serviceCollection = (IServiceCollection) new ServiceCollection();
+            startup.ConfigureServices(serviceCollection);
+            serviceCollection.AddTransient<ILoggerFactory, LoggerFactory>();
+            serviceCollection.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var controllerContext = new ControllerContext
+            {
+                ActionDescriptor = new ControllerActionDescriptor
+                {
+                    ControllerName = nameof(EmployeeController),
+                    ControllerTypeInfo = typeof(EmployeeController).GetTypeInfo()
+                },
+                HttpContext = new DefaultHttpContext
+                {
+                    RequestServices = serviceProvider
+                }
+            };
+
+            var controller = serviceProvider.GetService<IControllerFactory>().CreateController(controllerContext);
+
+            controller.ShouldBeOfType(typeof(EmployeeController));
+        }
+    }
+}

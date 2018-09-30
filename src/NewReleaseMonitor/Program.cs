b4dcc16074552d6e655f0d9f848ab15f233c00d8
Thomas Thiry <thomas.thiry@gmail.com>
2018-09-30 20:43:12 +0200

    Added NewReleaseMonitor

---
+++ src/NewReleaseMonitor/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading;
+
+namespace NewReleaseMonitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Monitoring started...");
+
+            var lastUpdate = GetLastUpdateTime();
+            
+            while (true)
+            {
+                Thread.Sleep(5000);
+
+                var newLastUpdate = GetLastUpdateTime();
+
+                if (newLastUpdate != lastUpdate)
+                {
+                    Process.Start(@"powershell", $@"-c (New-Object Media.SoundPlayer '../../../emergency030.wav').PlaySync();");
+                    Thread.Sleep(1500);
+                    Process.Start(@"powershell", $@"-c (New-Object Media.SoundPlayer '../../../NewVersion.wav').PlaySync();");
+
+                    lastUpdate = newLastUpdate;
+                }
+            }
+        }
+
+        private static string GetLastUpdateTime()
+        {
+            var httpClient = new HttpClient { BaseAddress = new Uri("http://partsunlimitedhrbenefits.azurewebsites.net") };
+
+            var response = httpClient.GetAsync("/lastupdate.txt").Result;
+
+            var lastUpdate = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine($"Last update: {lastUpdate}");
+            
+            return lastUpdate;
+        }
+    }
+}

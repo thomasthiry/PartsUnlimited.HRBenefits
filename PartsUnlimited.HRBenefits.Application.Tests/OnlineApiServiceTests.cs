6c748be94e88a2655b283dabf7fd8d3dc9aacc5f
Thomas Thiry <thomas.thiry@gmail.com>
2018-09-30 16:26:47 +0200

    Added PartsUnlimited.HRBenefits.Application.Tests

---
+++ PartsUnlimited.HRBenefits.Application.Tests/OnlineApiServiceTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace PartsUnlimited.HRBenefits.Application.Tests
+{
+    public class OnlineApiServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}

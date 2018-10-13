06889787559abdb98192bc92628356d59bc7a91c
Thomas Thiry <thomas.thiry@gmail.com>
2018-10-13 22:20:04 +0200

    Added EmployeeFileRepository

---
+++ src/PartsUnlimited.HRBenefits.Infrastructure/Repositories/EmployeeFileRepository.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using PartsUnlimited.HRBenefits.Domain.Entities;
+
+namespace PartsUnlimited.HRBenefits.Infrastructure.Repositories
+{
+    public class EmployeeFileRepository
+    {
+        private readonly string _filePath;
+
+        public EmployeeFileRepository(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Update(Employee employee)
+        {
+            var employees = ReadEmployees();
+
+            var employeeFromDb = employees.Single(e => e.Id == employee.Id);
+
+            employeeFromDb.Id = employee.Id;
+            employeeFromDb.Reference = employee.Reference;
+            employeeFromDb.LastName = employee.LastName;
+            employeeFromDb.FirstName = employee.FirstName;
+            employeeFromDb.DateOfBirth = employee.DateOfBirth;
+            employeeFromDb.AddressNumber = employee.AddressNumber;
+            employeeFromDb.AddressStreet = employee.AddressStreet;
+            employeeFromDb.AddressCity = employee.AddressCity;
+            employeeFromDb.AddressPostalCode = employee.AddressPostalCode;
+            employeeFromDb.AddressCountry = employee.AddressCountry;
+            employeeFromDb.JoinedCompanyDate = employee.JoinedCompanyDate;
+            employeeFromDb.GrossMonthlySalary = employee.GrossMonthlySalary;
+            employeeFromDb.IsGrantedCar = employee.IsGrantedCar;
+            employeeFromDb.NbDaysYearlyHolidays = employee.NbDaysYearlyHolidays;
+
+            var jsonToWrite = JsonConvert.SerializeObject(employees);
+            
+            File.WriteAllText(_filePath, jsonToWrite);
+        }
+
+        private List<Employee> ReadEmployees()
+        {
+            var json = File.ReadAllText(_filePath);
+            var employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+
+            return employees;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            if (File.Exists(_filePath) == false)
+            {
+                return null;
+            }
+
+            var employees = ReadEmployees();
+
+            return employees.Single(e => e.Id == id);
+        }
+    }
+}
---
+++ tests/PartsUnlimited.HRBenefits.Infrastructure.IntegrationTests/EmployeeFileRepositoryTests.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using PartsUnlimited.HRBenefits.Domain.Entities;
+using PartsUnlimited.HRBenefits.Infrastructure.Repositories;
+using Shouldly;
+using Xunit;
+
+namespace PartsUnlimited.HRBenefits.Infrastructure.IntegrationTests
+{
+    public class EmployeeFileRepositoryTests
+    {
+        private const string FilePath = "employees.json";
+
+        private static EmployeeFileRepository CreateRepository()
+        {
+            return new EmployeeFileRepository(FilePath);
+        }
+
+        [Fact]
+        public void Update()
+        {
+            var repository = CreateRepository();
+
+            repository.Update(new Employee
+            {
+                Id = 1,
+                FirstName = "Jean-Claude",
+                LastName = "Van Damme"
+            });
+
+            var employee = repository.GetEmployee(1);
+            employee.FirstName.ShouldBe("Jean-Claude");
+
+            repository.Update(new Employee
+            {
+                Id = 1,
+                FirstName = "Memorial",
+                LastName = "Van Damme"
+            });
+
+            employee = repository.GetEmployee(1);
+            employee.FirstName.ShouldBe("Memorial");
+        }
+
+        [Fact]
+        public void GetEmployee()
+        {
+            var repository = CreateRepository();
+
+            var employee = repository.GetEmployee(1);
+
+            employee.LastName.ShouldBe("Van Damme");
+        }
+
+        [Fact]
+        public void GetEmployee_FileDoesNotExist_ReturnsNull()
+        {
+            var repository = new EmployeeFileRepository("non-existing-file.json");
+
+            var employee = repository.GetEmployee(1);
+
+            employee.ShouldBeNull();
+        }
+    }
+}

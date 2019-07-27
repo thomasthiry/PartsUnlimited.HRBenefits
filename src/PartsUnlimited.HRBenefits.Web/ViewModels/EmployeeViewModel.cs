53d9a65903124ba8a014967ce0327e29de4d5b99
Thomas Thiry <thomas.thiry@gmail.com>
2019-07-27 16:51:56 +0200

    Let aspnet decide field type for date fields on employee edit view in order to help with different date formats

--- src/PartsUnlimited.HRBenefits.Web/ViewModels/EmployeeViewModel.cs
+++ src/PartsUnlimited.HRBenefits.Web/ViewModels/EmployeeViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.HRBenefits.Web.ViewModels
 {
@@ -8,12 +9,14 @@ namespace PartsUnlimited.HRBenefits.Web.ViewModels
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Reference { get; set; }
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
         public string AddressNumber { get; set; }
         public string AddressStreet { get; set; }
         public string AddressCity { get; set; }
         public string AddressPostalCode { get; set; }
         public string AddressCountry { get; set; }
+        [DataType(DataType.Date)]
         public DateTime JoinedCompanyDate { get; set; }
         public decimal GrossMonthlySalary { get; set; }
         public bool IsGrantedCar { get; set; }

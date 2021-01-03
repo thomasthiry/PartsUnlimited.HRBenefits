37d9304de5fca707cf4ec80d29c737933b90680d
Thomas Thiry <thomas.thiry@gmail.com>
2021-01-03 21:55:08 +0100

    Hardcoded path to git as PATH variable is not up-to-date in the script right after choco installation

--- LaunchDevTools.ps1
+++ LaunchDevTools.ps1
@@ -1,4 +1,4 @@
-& C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits\PartsUnlimited.HRBenefits.sln
+& "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits\PartsUnlimited.HRBenefits.sln
 & C:\Users\TddWorkshop\AppData\Local\Fork\Fork.exe
 explorer C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits
 Start-Process C:\tools\cmdermini\vendor\conemu-maximus5\ConEmu64.exe -WorkingDirectory "C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits"

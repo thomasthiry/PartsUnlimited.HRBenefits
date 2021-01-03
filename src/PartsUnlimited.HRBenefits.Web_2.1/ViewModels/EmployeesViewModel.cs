fb18b88567890a2cc2d768069d43bedafcf2b023
Thomas Thiry <thomas.thiry@gmail.com>
2021-01-03 15:05:41 +0100

    Deleted aspnet 2.1 web project (replaced by aspnet 5.0

--- src/PartsUnlimited.HRBenefits.Web_2.1/ViewModels/EmployeesViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace PartsUnlimited.HRBenefits.Web.ViewModels
-{
-    public class EmployeesViewModel
-    {
-        public IEnumerable<EmployeeViewModel> Employees;
-    }
-}

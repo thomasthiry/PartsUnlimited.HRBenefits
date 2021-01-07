3fef008cca6084c78a93ed594c974f11f7930a4a
Thomas Thiry <thomas.thiry@gmail.com>
2021-01-07 16:16:04 +0100

    Fix launch Edge and cmder

--- LaunchDevTools.ps1
+++ LaunchDevTools.ps1
@@ -1,5 +1,5 @@
 & "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits\PartsUnlimited.HRBenefits.sln
 & C:\Users\TddWorkshop\AppData\Local\Fork\Fork.exe
 explorer C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits
-Start-Process C:\tools\cmdermini\vendor\conemu-maximus5\ConEmu64.exe -WorkingDirectory "C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits"
-& "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" "https://miro.com/app/board/o9J_lcheR4k=/"
+C:\tools\cmdermini\Cmder.exe  /start "C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits"
+start microsoft-edge:https://miro.com/app/board/o9J_lcheR4k=/

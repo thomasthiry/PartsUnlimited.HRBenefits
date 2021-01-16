4dbfff461b0d606bf7a0f7d874b03c3b85b16d79
Thomas Thiry <thomas.thiry@gmail.com>
2021-01-16 18:10:52 +0100

    Update miro startup url

--- LaunchDevTools.ps1
+++ LaunchDevTools.ps1
@@ -2,4 +2,5 @@
 & C:\Users\TddWorkshop\AppData\Local\Fork\Fork.exe
 explorer C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits
 C:\tools\cmdermini\Cmder.exe  /start "C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits"
-start microsoft-edge:https://miro.com/app/board/o9J_lcheR4k=/
+start microsoft-edge:https://miro.com/app/board/o9J_lcheR4k=/?moveToWidget=3074457352800859261&cot=14
+& "C:\Program Files (x86)\AnyDeskMSI\AnyDeskMSI.exe"

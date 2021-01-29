52bf6951ed22df2e76599f2a6678b88373d43e3b
Thomas Thiry <thomas.thiry@gmail.com>
2021-01-29 20:44:49 +0100

    Updated link to Miro board

--- LaunchDevTools.ps1
+++ LaunchDevTools.ps1
@@ -2,5 +2,5 @@
 & C:\Users\TddWorkshop\AppData\Local\Fork\Fork.exe
 explorer C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits
 C:\tools\cmdermini\Cmder.exe  /start "C:\Users\TddWorkshop\Dev\PartsUnlimited.HRBenefits"
-start microsoft-edge:https://miro.com/app/board/o9J_lcheR4k=/?moveToWidget=3074457352800859261
+start microsoft-edge:https://miro.com/app/board/o9J_lWhAC_M=/?moveToWidget=3074457353771709244
 & "C:\Program Files (x86)\AnyDeskMSI\AnyDeskMSI.exe"
